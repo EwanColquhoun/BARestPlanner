207791422a3c1696771d870e267f73e1ca248541
EwanColquhoun <ewancolquhoun@hotmail.com>
2023-04-07 12:26:31 +0000

    Fixed revised on block times

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ let raw_dest = document.getElementById('dest')
 let raw_eobt = document.getElementById('eobt') 
 let raw_toTime = document.getElementById('taxiOutTime') 
 let raw_tiTime = document.getElementById('taxiInTime') 
-
 let crewDiv = document.getElementById('crew-content')
-
+let extra = false
 let calc = document.getElementById('calc')
 let results = document.getElementById('results-p')
+let newResults = document.getElementById('new-results-p')
 
 let sixToEight = {1:"12:00", 2:"11:15", 3:"10:30", 4:"09:45"}
 let eightToOne = {1:"12:00", 2:"12:00", 3:"12:00", 4:"11:15"}
@@ -51,6 +51,7 @@ function init(raw_crew, raw_sectors){
 
 function calculate (){
     let repTime = raw_repTime.value
+    let zRepTime = hasDST(repTime)
     // let blockTime = raw_blockTime.value
     let fltTime = raw_fltTime.value
     let toTime = raw_toTime.value
@@ -60,37 +61,62 @@ function calculate (){
     let dest = raw_dest.value
     let eobt = raw_eobt.value
     let sectors = raw_sectors.value
-    // console.log(repTime, blockTime, acc, sectors, crew, dest, eobt)
+
     let blockTime = getBlockTime(fltTime, tiTime, toTime)
     displayBlockTime(blockTime)
-    let mFdp = fdp(repTime, sectors, dest, crew)
-    console.log(mFdp, 'mfdp')
-    let [latest, dur] = latestOnBlock(repTime, blockTime, mFdp)
-    console.log(latest, dur, 'latest, dur')
-    let lastTot = lastOffBlocks(latest, blockTime)
-    let [pilots, newFdp, restRqd, predFDP] = extraPilot(mFdp, eobt, blockTime, repTime, crew)
-    let lastTotRevised = lastOffBlocksRevised(mFdp, newFdp, blockTime, lastTot)
+    console.log(repTime, blockTime, acc, sectors, crew, dest, eobt)
+    let mFdp = fdp(zRepTime, sectors, dest, crew)
+    // console.log(mFdp, 'mfdp')
+    let latest = latestOnBlock(zRepTime, mFdp)
+    // console.log(latest, dur, 'latest, dur')
+    let lastPush = lastOffBlocks(latest, blockTime)
+    let [pilots, newFdp, restRqd, predFDP, extra] = extraPilot(mFdp, eobt, blockTime, zRepTime, crew)
     // console.log(pilots, 'pilots')
-    display(repTime,blockTime,acc,mFdp, latest, lastTot, pilots, newFdp, restRqd, predFDP, lastTotRevised)
-
+    display(blockTime,acc,mFdp, latest, lastPush)
+    if (extra == true) {
+        let lastTotRevised = lastOffBlocksRevised(mFdp, newFdp, blockTime, lastPush, zRepTime)
+        console.log(extra, 'extra')
+        console.log(newFdp, 'extra NEW FDP')
+        console.log(mFdp, 'extra NEW mFDP')
+        newDisplay( pilots, newFdp, restRqd, predFDP, lastTotRevised);
+    } else {
+        console.log(extra, 'NO extra')
+        noExtra(newFdp, lastPush);
+    }
+    
 };
 
-function display (repTime,blockTime,acc,mFdp, latest, lastTot, pilots, newFdp, restRqd, predFDP, lastOffBlocksRevised) {
+function display (blockTime,acc,mFdp, latest, lastPush) {
     results.innerHTML = `<span>
-    Report Time (Local): ${repTime}<br>
+    Report Time (Local): ${raw_repTime.value}<br>
     Block time:${blockTime}.<br>
     Acclimatised: ${acc}<br>
     MAX FDP: ${mFdp}<br>
-    Latest off blocks ${lastTot}<br>
-    Latest on blocks: ${latest}<br>
+    Latest off blocks ${lastPush}z<br>
+    Latest on blocks: ${latest}z<br>
+    </span>
+    `
+}
+
+function newDisplay (pilots, newFdp, restRqd, predFDP, lastOffBlocksRevised){
+    console.log('new Display')
+    newResults.innerHTML = `<span>
     <br>
     Required Crew compliment: ${pilots}<br>
     New FDP: ${predFDP}<br>
     New max FDP: ${newFdp}<br>
-    Revised latest off blocks: ${lastOffBlocksRevised}<br>
+    Revised latest off blocks: ${lastOffBlocksRevised}z<br>
     Rest Required: ${restRqd}<br>
-    </span>
-    `
+    </span>`
+}
+
+function noExtra(newFdp, lastPush){
+    newResults.innerHTML = `<span>
+    <br>
+    No additional crew required <br>
+    Max FDP: ${newFdp}<br>
+    Latest off blocks: ${lastPush}<br>
+    </span>`
 }
 
 function displayBlockTime(blockTime){
@@ -116,7 +142,6 @@ function populateCrew(raw_crew){
     <p>${crew}</p>`
 }
 
-
 function getBlockTime(fltTime, tiTime, toTime){
     let splitTin = tiTime.split(':')
     let luxontoTime = luxon.Duration.fromISOTime(toTime).toObject()
@@ -128,185 +153,186 @@ function getBlockTime(fltTime, tiTime, toTime){
     return total
 }
 
-
 function fdp(rt, s, dest, crew){
-    let report = ''
+    let mfdpCalc = ''
     if (crew == "2"){
         if (dest == "USA & Canada") {
             if ("06:00" <= rt && rt <= "07:59"){
-                report = sixToEight[s]
-                return report
+                mfdpCalc = sixToEight[s]
+                return mfdpCalc
             } else if ("0800" <= rt && rt <= "12:59"){
-                report = eightToOne[s]
-                return report
+                mfdpCalc = eightToOne[s]
+                return mfdpCalc
             } else if ("1300" <= rt && rt <= "17:59"){
-                report = oneToSix[s]
-                return report
+                mfdpCalc = oneToSix[s]
+                return mfdpCalc
             } else if ("1800" <= rt && rt <= "21:59"){
-                report = sixToTen[s]
-                return report
+                mfdpCalc = sixToTen[s]
+                return mfdpCalc
             } else {
-                report = tenToSix[s]
-                return report
+                mfdpCalc = tenToSix[s]
+                return mfdpCalc
             }
         } else {
             if ("06:00" <= rt && rt <= "07:59"){
-                report = sixToEightOther[s]
-                return report
+                mfdpCalc = sixToEightOther[s]
+                return mfdpCalc
             } else if ("0800" <= rt && rt <= "12:59"){
-                report = eightToOneOther[s]
-                return report
+                mfdpCalc = eightToOneOther[s]
+                return mfdpCalc
             } else if ("1300" <= rt && rt <= "17:59"){
-                report = oneToSixOther[s]
-                return report
+                mfdpCalc = oneToSixOther[s]
+                return mfdpCalc
             } else if ("1800" <= rt && rt <= "21:59"){
-                report = sixToTenOther[s]
-                return report
+                mfdpCalc = sixToTenOther[s]
+                return mfdpCalc
             } else {
-                report = tenToSixOther[s]
-                return report
+                mfdpCalc = tenToSixOther[s]
+                return mfdpCalc
             }
         } 
     } else if (crew == "3"){
         if (dest == "USA & Canada") {
             if ("06:00" <= rt && rt <= "07:59"){
-                report = luxon.DateTime.fromISO(sixToEight[s]).plus({hour: '3'}).toFormat('T')
-                return report
+                mfdpCalc = luxon.DateTime.fromISO(sixToEight[s]).plus({hour: '3'}).toFormat('T')
+                return mfdpCalc
             } else if ("0800" <= rt && rt <= "12:59"){
-                report = luxon.DateTime.fromISO(eightToOne[s]).plus({hour: '3'}).toFormat('T')
-                return report
+                mfdpCalc = luxon.DateTime.fromISO(eightToOne[s]).plus({hour: '3'}).toFormat('T')
+                return mfdpCalc
             } else if ("1300" <= rt && rt <= "17:59"){
-                report = luxon.DateTime.fromISO(oneToSix[s]).plus({hour: '3'}).toFormat('T')
-                return report
+                mfdpCalc = luxon.DateTime.fromISO(oneToSix[s]).plus({hour: '3'}).toFormat('T')
+                return mfdpCalc
             } else if ("1800" <= rt && rt <= "21:59"){
-                report = luxon.DateTime.fromISO(sixToTen[s]).plus({hour: '3'}).toFormat('T')
-                return report
+                mfdpCalc = luxon.DateTime.fromISO(sixToTen[s]).plus({hour: '3'}).toFormat('T')
+                return mfdpCalc
             } else {
-                report = luxon.DateTime.fromISO(tenToSix[s]).plus({hour: '3'}).toFormat('T')
-                return report
+                mfdpCalc = luxon.DateTime.fromISO(tenToSix[s]).plus({hour: '3'}).toFormat('T')
+                return mfdpCalc
             }
         } else {
             if ("06:00" <= rt && rt <= "07:59"){
-                report = sixToEightOther[s]
-                return report
+                mfdpCalc = luxon.DateTime.fromISO(sixToEightOther[s]).plus({hour: '3'}).toFormat('T')
+                return mfdpCalc
             } else if ("0800" <= rt && rt <= "12:59"){
-                report = eightToOneOther[s]
-                return report
+                mfdpCalc = luxon.DateTime.fromISO(eightToOneOther[s]).plus({hour: '3'}).toFormat('T')
+                return mfdpCalc
             } else if ("1300" <= rt && rt <= "17:59"){
-                report = oneToSixOther[s]
-                return report
+                mfdpCalc = luxon.DateTime.fromISO(oneToSixOther[s]).plus({hour: '3'}).toFormat('T')
+                return mfdpCalc
             } else if ("1800" <= rt && rt <= "21:59"){
-                report = sixToTenOther[s]
-                return report
+                mfdpCalc = luxon.DateTime.fromISO(sixToTenOther[s]).plus({hour: '3'}).toFormat('T')
+                return mfdpCalc
             } else {
-                report = tenToSixOther[s]
-                return report
+                mfdpCalc = luxon.DateTime.fromISO(tenToSixOther[s]).plus({hour: '3'}).toFormat('T')
+                return mfdpCalc
             }
         }
     } if (crew == "4"){
-        report = "18:00"
-        return report
+        mfdpCalc = "18:00"
+        return mfdpCalc
     } 
 }
 
-function latestOnBlock(repTime, blockTime, mFdp){
+function latestOnBlock(repTime, mFdp){
     let max = mFdp.split(':');
 
     let mFdpTime = luxon.Duration.fromObject({hours: max[0], minutes: max[1]})
     let newDur = luxon.Duration.fromISO(mFdpTime).minus({hours: '0', minutes: '30'})
-
     let latestBlock = luxon.DateTime.fromISO(repTime).plus(newDur).toFormat('T')
-    return [latestBlock, newDur]
+    return latestBlock
 }
 
 function lastOffBlocks(latest, blockTime){
 
     let splitBlock = blockTime.split(':');
     let dur = luxon.Duration.fromObject({hours: splitBlock[0], minutes: splitBlock[1]})
-    let lastToTime = luxon.DateTime.fromISO(latest).minus(dur).toFormat('T')
-    
-    
-    console.log(lastToTime, 'last TO time')
-
-    return lastToTime
+    let lastPushTime = luxon.DateTime.fromISO(latest).minus(dur).toFormat('T')
+ 
+    return lastPushTime
 }
 
-function lastOffBlocksRevised(mFdp, newFdp, blockTime, lastTot){
+function lastOffBlocksRevised(mFdp, newFdp, blockTime, lastPush, repTime){
     let lastBlockTime = '';
-    
-    if (mFdp = newFdp){
-        lastBlockTime = lastTot
+    if (mFdp == newFdp){
+        lastBlockTime = lastPush
     } else {
-   
-        let splitBlock = blockTime.split(':');
-        let dur = luxon.Duration.fromObject({hours: splitBlock[0], minutes: splitBlock[1]})
-        lastBlockTime = luxon.DateTime.fromISO(newFdp).minus(dur).toFormat('T')
+        let newOn = latestOnBlock(repTime, newFdp)
+        lastBlockTime = lastOffBlocks(newOn, blockTime)
     }
-    
-    
-    console.log(lastBlockTime, 'last block time')
-
     return lastBlockTime
 }
 
-
-function extraPilot(mFdp, eobt, blockTime, reportTime, crew){
+function extraPilot(mFdp, eobt, blockTime, zRep, crew){
 	
-	let boxA = luxon.DateTime.fromISO(mFdp).plus({minutes: '30'}).toFormat('T')
-	let boxB = luxon.DateTime.fromISO(boxA).plus({hour: '1'}).toFormat('T')
-	let boxC = luxon.DateTime.fromISO(boxA).plus({hour: '3'}).toFormat('T')
-	let boxD = luxon.DateTime.fromISO(mFdp).plus({hour: '5'}).toFormat('T')
+    let boxA = ''
+    let boxB = ''
+    let boxC = ''
+    if (crew == '2'){
+        boxA = luxon.DateTime.fromISO(mFdp).plus({minutes: '30'}).toFormat('T')
+        boxB = luxon.DateTime.fromISO(boxA).plus({hour: '1'}).toFormat('T')
+        boxC = luxon.DateTime.fromISO(boxA).plus({hour: '3'}).toFormat('T')
+        console.log(boxA, boxB, boxC)
+    }
+	// let boxD = luxon.DateTime.fromISO(mFdp).plus({hour: '5'}).toFormat('T')
 	let restRqd = '' 		//maybe needs these three variables out of function scope.
 	let newFdp = ''
-	let rcmd = {}
     let nCrew = ''
-    let estblock = eobt.split(':');
 
-    console.log(eobt, 'eobt')
-    console.log(mFdp, 'mfdp')
-    console.log(blockTime, 'blocktime')
+    // console.log(eobt, 'eobt')
+    // console.log(mFdp, 'mfdp from inside extra')
+    // console.log(blockTime, 'blocktime')
     let splitBlock = blockTime.split(':')
     console.log(splitBlock, 'splitblock')
     let durBlock = luxon.Duration.fromObject({hours: splitBlock[0], minutes: splitBlock[1]})
-    // predicted duty time =  (eobt+block) - report time
+    // predicted duty time =  (eobt+block) - mfdpCalc time
     console.log(durBlock, 'durblock')
-
+    console.log(eobt, 'eobt')
     let eobtBlock = luxon.DateTime.fromISO(eobt).plus(durBlock).toFormat('T') //ERROR with the duration of the block time.
     console.log(eobtBlock, 'eobtBlock')
-    console.log('report', reportTime)
-
-    let pfdp = getDiff(eobtBlock, reportTime)
-    let luxonPFDP = luxon.DateTime.fromISO(pfdp).toFormat('T')
-    console.log(luxonPFDP, 'luxonpfdp')
+    // let zmfdp = hasDST(mfdpCalcTime)
+    let pfdp = getDiff(eobtBlock, zRep)
+    // let luxonPFDP = luxon.DateTime.fromISO(pfdp).toFormat('T')
+    // console.log(luxonPFDP, 'luxonpfdp')
+    console.log(pfdp, 'pfdp')
+    console.log(mFdp, 'mfdp')
 
     if (crew=='2' && pfdp > mFdp && pfdp <= boxA){
-		newFdp = boxA
+		newFdp = mFdp
 		restRqd = 'Minimum rest 12 hours.'
 		nCrew = '3rd Pilot'
         console.log('2A')
+        extra = true
 	} else if (crew=='2' && pfdp > boxA && pfdp <= boxB){
-		newFdp = boxB
+		newFdp = mFdp
 		restRqd = '2LN after flight.'
 		nCrew = '3rd Pilot'
         console.log('2b')
-	} else if (crew=='2' && pfdp > boxB && pfdp <=boxC){
-		newFdp = boxC
+        extra = true
+	} else if (crew=='2' && pfdp > boxB && pfdp <= boxC){
+		newFdp = mFdp
 		restRqd = '2LN before if time zone change greater than 5 hours. 2LN after.'
 		nCrew = '3rd Pilot'
         console.log('2c')
-	} else if (crew=='3' && pfdp > boxB && pfdp <=boxC){
-		newFdp = boxD
+        extra = true
+	} else if (crew=='3' && pfdp > mFdp){
+		newFdp = "18:00"
 		restRqd = '2LN before and after.'
 		nCrew = '4th Pilot'
         console.log('3')
-	} else if (crew=='2' && pfdp < mFdp && pfdp <= boxA) {
+        extra = true
+	} else if (crew=='2' && pfdp < mFdp) {
         newFdp = mFdp
 		restRqd = 'Minimum rest 12 hours.'
 		nCrew = '2 Pilots'
         console.log('2')
+    } else {
+        newFdp = mFdp
+		restRqd = 'Minimum rest 12 hours/Or FDP if longer'
+		nCrew = 'Current'
+        console.log('else')
     }
 
-    return [nCrew, newFdp, restRqd, pfdp] 
+    return [nCrew, newFdp, restRqd, pfdp, extra]
 }
 
 
@@ -323,17 +349,22 @@ function getDiff(time1, time2){
 
          var timeDiff = timeEnd - timeStart;
          if (timeDiff < 0) {
-            console.log('top')
+            // console.log('top')
             timeDiff = 86400000 + timeDiff;
             hours = timeDiff/3600000
-            let timeString = hours.toString().split('.')
-            hours = timeString[0]
-            let longMin = timeString[1] *6
-            minutes = longMin.toString().slice(0,2)
-            // minutes = Math.trunc(longMin)
-            console.log(longMin, 'longmin')
+            if (hours % 1 == '0'){
+                minutes = '00'
+            } else {
+                let timeString = hours.toString().split('.')
+                // console.log(timeString, 'ts')
+                hours = timeString[0]
+                let longMin = timeString[1] *6
+                minutes = longMin.toString().slice(0,2)
+                // minutes = Math.trunc(longMin)
+                // console.log(longMin, 'longmin')
+            }
          } else {
-            console.log('bottom')
+            // console.log('bottom')
 
             hours = timeDiff/3600000
             let timeString = hours.toString().split('.')
@@ -345,4 +376,17 @@ function getDiff(time1, time2){
         console.log(totalDiff, 'predicted fdp')
         
         return totalDiff
+}
+
+function hasDST(mfdpCalcTime){
+    let z = luxon.DateTime.fromISO(mfdpCalcTime)
+    if (z.isInDST) {
+        let zRep = luxon.DateTime.fromISO(mfdpCalcTime).minus(3600000).toFormat('T')
+        console.log('DST')
+        return zRep
+    } else {
+        console.log('no dst')
+        return z
+    }
+
 }
